f0fc3d936d62ea50e7b3812d677249b92d867869
devim <developerimalka@gmail.com>
2024-06-05 00:42:38 +0530

    Add file extension check  -- file extension checking added  -- file not found error handled  -- static file loading added

--- filehandle.c
+++ filehandle.c
@@ -1,125 +1,116 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdint.h>
 
 #define BUFFSIZE 2048
 
 typedef struct file_data {
-	uint8_t *data;
-	size_t length;
-}file_data;
+  uint8_t *data;
+  size_t length;
+} file_data;
 
+struct file_data *read_file(const char *path) {
 
-struct file_data *read_file(const char *path){
+  FILE *handler = NULL;
+  struct file_data *fdata = NULL;
+  size_t read_length = 0;
+  uint8_t buffer[BUFFSIZE] = {0x00};
 
-		FILE *handler = NULL;
-		struct file_data *fdata = NULL;
-		size_t read_length = 0;
-		uint8_t buffer[BUFFSIZE] = {0x00};
+  fdata = malloc(sizeof(struct file_data));
+  if (fdata == NULL)
+    return NULL;
 
-		fdata = malloc(sizeof(struct file_data));
-		if(fdata == NULL)
-			return NULL;
+  bzero(fdata, sizeof(struct file_data));
 
-		bzero(fdata, sizeof(struct file_data));
+  fdata->data = malloc(1);
+  if (fdata->data == NULL) {
+    free(fdata);
+    return NULL;
+  }
 
-		fdata->data = malloc(1);
-		if(fdata->data == NULL){
-			free(fdata);
-			return NULL;
-		}
+  handler = fopen(path, "rb");
+  if (handler == NULL) {
+    free(fdata->data);
+    free(fdata);
+    return NULL;
+  }
 
-		handler = fopen(path,"rb");
-		if(handler == NULL){
-			free(fdata->data);
-			free(fdata);
-			return NULL;
-		}
+  while (!feof(handler)) {
+    read_length = fread(buffer, 1, BUFFSIZE, handler);
+    fdata->data = realloc(fdata->data, fdata->length + read_length);
+    if (fdata->data == NULL) {
+      fclose(handler);
+      free(fdata);
+      return NULL;
+    }
+    memcpy(fdata->data + fdata->length, buffer, read_length);
+    fdata->length += read_length;
+  }
 
-		while(!feof(handler)){
-			read_length = fread(buffer,1,BUFFSIZE,handler);
-			fdata->data = realloc(fdata->data,fdata->length + read_length);
-			if(fdata->data == NULL){
-				fclose(handler);
-				free(fdata);
-				return NULL;
-			}
-			memcpy(fdata->data +fdata->length, buffer, read_length);
-			fdata->length += read_length;
-		}
+  fclose(handler);
 
-		fclose(handler);
-
-		return fdata;
+  return fdata;
 }
 
-
 // open the requested file and return the file pointer
-FILE* requestfile(char *filename){
-	FILE *fp;
-	fp = fopen(filename,"r");
-	if(fp != NULL){
-		return fp;
-	}
-	else{
-		perror("Error opening file\n");
-	}
+FILE *requestfile(char *filename) {
+  FILE *fp;
+  fp = fopen(filename, "r");
+  if (fp != NULL) {
+    return fp;
+  } else {
+    perror("Error opening file\n");
+  }
 }
 
 // count the byte size of file
-int count_file_bytes(FILE *fp)
-{
-	int c,count = 0;
-	if(fp != NULL){
-		while((c = fgetc(fp)) != EOF)
-			count++;
-	}
-	fclose(fp);
-	return count;
+int count_file_bytes(FILE *fp) {
+  int c, count = 0;
+  if (fp != NULL) {
+    while ((c = fgetc(fp)) != EOF)
+      count++;
+  }
+  fclose(fp);
+  return count;
 }
 
 // read the file content to buffer and return
-char* content(char *filename)
-{
-	FILE *fp = requestfile(filename);
-    if(fp == NULL)
-        return "404 Not Found";
-	int bytecount = count_file_bytes(fp);
-	char *content = (char *) malloc(bytecount+1);
+char *content(char *filename) {
+  FILE *fp = requestfile(filename);
+  if (fp == NULL) {
+    char *content = "404 Not Found";
 
-	FILE *file = requestfile(filename);
-	size_t i;
+  } else {
+    int bytecount = count_file_bytes(fp);
+    char *content = (char *)malloc(bytecount + 1);
 
-	for(i=0;i<(bytecount+1);++i)
-	{
-		int c = fgetc(file);
-		if( c == EOF)
-		{
-			break;
-		}
+    FILE *file = requestfile(filename);
+    size_t i;
 
-		content[i]= c;
-	}
+    for (i = 0; i < (bytecount + 1); ++i) {
+      int c = fgetc(file);
+      if (c == EOF) {
+        break;
+      }
 
-	content[i] = '\0';
-	fclose(file);
-	return content;
+      content[i] = c;
+    }
 
-
-
+    content[i] = '\0';
+    fclose(file);
+    return content;
+  }
 }
 
 // get file extension before last '.'
-char *get_file_ext(char *filename, int c)
-{
-	char *type = strrchr(filename,c);
-	return type;
+char *get_file_ext(char *filename, int c) {
+  char *type = strrchr(filename, c);
+  return type;
 }
 
-
 // read the file in binary format
-//char *read_binary_file(char *filename)
+// char *read_binary_file(char *filename)
 //{
 //	FILE *fp = fopen(filename,"rb");
 //	if(fp == NULL)
--- main.c
+++ main.c
@@ -10,127 +10,184 @@
 #define PORT "8080"
 #define BUFFSIZE 2048
 #define RESSIZE 4096
+
 #include "filehandle.h"
 #include "httpresponse.h"
-
-char *get_file_path(const char *request) {
-    char *path = NULL;
-    char *start = strstr(request, "GET ");
-    if (start != NULL) {
-	start += 4; // Move past "GET "
-	char *end = strstr(start, " ");
-	if (end != NULL) {
-	    int path_length = end - start;
-	    path = (char *)malloc(path_length + 1);
-	    strncpy(path, start, path_length);
-	    path[path_length] = '\0';
-	}
-    }
-    return path;
-};
-
-char *requestpath(int sockfd) {
-    char *buffer = (char *)malloc(BUFFSIZE);
-
-    int bytes = read(sockfd, buffer, BUFFSIZE);
-    return buffer;
-};
+#include "routes.h"
 
 int main(int argc, char *argv[]) {
 
-    int status;
-    struct addrinfo hints, *res;
-    int sockfd;
-    int bindstatus;
+  int status;
+  struct addrinfo hints, *res;
+  int sockfd;
+  int bindstatus;
 
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_STREAM;
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_STREAM;
 
-    status = getaddrinfo(NULL, PORT, &hints, &res);
-    if (status == -1) {
-	fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+  status = getaddrinfo(NULL, PORT, &hints, &res);
+  if (status == -1) {
+    fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+  }
+
+  sockfd = socket(res->ai_family, res->ai_socktype, 0);
+  if (sockfd == -1) {
+    fprintf(stderr, "socket error %s\n", strerror(sockfd));
+  }
+
+  int opt = 1;
+  setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt));
+
+  bindstatus = bind(sockfd, res->ai_addr, res->ai_addrlen);
+  if (bindstatus == -1) {
+    fprintf(stderr, "bind error %s\n", strerror(bindstatus));
+  }
+
+  freeaddrinfo(res);
+
+  int l;
+  l = listen(sockfd, 10);
+  if (l != 0) {
+    fprintf(stderr, "LISTEN ERROR %s\n", strerror(l));
+  }
+
+  while (1) {
+    struct sockaddr_in client_addr;
+    int newfd;
+    socklen_t theiraddr = sizeof client_addr;
+    newfd = accept(sockfd, (struct sockaddr *)&client_addr, &theiraddr);
+
+    if (newfd == -1) {
+      fprintf(stderr, "accept error %s\n", strerror(errno));
+    } else {
+      char str[INET_ADDRSTRLEN];
+      inet_ntop(client_addr.sin_family, (struct inaddr *)&client_addr.sin_addr,
+                str, INET_ADDRSTRLEN);
+      printf("Connection from %s\n", str);
     }
 
-    sockfd = socket(res->ai_family, res->ai_socktype, 0);
-    if (sockfd == -1) {
-	fprintf(stderr, "socket error %s\n", strerror(sockfd));
-    }
+    char *request = requestpath(newfd);
+    char *filepath = get_file_path(request);
+    free(request);
 
-    int opt = 1;
-    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt));
+    char *ext = get_file_ext(filepath, '.');
+    printf("file ext is %s\n", ext);
 
-    bindstatus = bind(sockfd, res->ai_addr, res->ai_addrlen);
-    if (bindstatus == -1) {
-	fprintf(stderr, "bind error %s\n", strerror(bindstatus));
-    }
+    char *path = removenslash(filepath);
+    printf("path is %s\n", path);
 
-    freeaddrinfo(res);
+    char *response = NULL;
 
-    int l;
-    l = listen(sockfd, 10);
-    if (l != 0) {
-	fprintf(stderr, "LISTEN ERROR %s\n", strerror(l));
-    }
+    if (ext == NULL && strcmp(filepath, "/") == 0 ||
+        strcmp(ext, ".html") == 0) {
+      if (strcmp(path, "") == 0 || strcmp(filepath, "index.html") == 0) {
 
-    while (1) {
-	struct sockaddr_in client_addr;
-	int newfd;
-	socklen_t theiraddr = sizeof client_addr;
-	newfd = accept(sockfd, (struct sockaddr *)&client_addr, &theiraddr);
-
-	if (newfd == -1) {
-	    fprintf(stderr, "accept error %s\n", strerror(errno));
-	} else {
-	    char str[INET_ADDRSTRLEN];
-	    inet_ntop(client_addr.sin_family, (struct inaddr *)&client_addr.sin_addr,
-		    str, INET_ADDRSTRLEN);
-	    printf("connection from %s\n", str);
-	}
-
-	char *request = requestpath(newfd);
-	char *filepath = get_file_path(request);
-	printf("%s\n", filepath);
-	free(request);
-
-	char *response = NULL;
-
-    if (strcmp(filepath, "/") == 0 || strcmp(filepath, "/index.html") == 0) {
         response = content("index.html");
-
-        char *http_response = (char *) malloc(RESSIZE);
+        char *http_response = (char *)malloc(RESSIZE);
         char *date = get_date_for_server();
         int content_length = strlen(response);
-        int len,bytes_sent;
-        int n = snprintf(http_response,RESSIZE,"HTTP/1.1 200 OK\r\nAcess-Control-Allow-Origin: *\r\nConnection: Keep-Alive\r\nDate: %s\r\nContent-Type: %s\r\nContent-Length: %llu\r\n\n%s",date,"text/html",content_length,response);
+        int len, bytes_sent;
+        int n =
+            snprintf(http_response, RESSIZE,
+                     "HTTP/1.1 200 OK\r\nAcess-Control-Allow-Origin: "
+                     "*\r\nConnection: Keep-Alive\r\nDate: %s\r\nContent-Type: "
+                     "%s\r\nContent-Length: %d\r\n\n%s",
+                     date, "text/html", content_length, response);
         len = strlen(http_response);
-        bytes_sent = send(newfd,http_response,len,0);
+        bytes_sent = send(newfd, http_response, len, 0);
+        shutdown(newfd, SHUT_WR);
         free(date);
         free(http_response);
 
-        shutdown(newfd, SHUT_WR);
-    }
-    else if(strcmp(filepath,"/image.jpg") == 0){
-        char http_header[256];
+      } else {
+        response = content(path);
+        char *statuscode = "200 OK";
+        if (strcmp(response, "404 Not Found") == 0)
+          statuscode = "404 Not Found";
+        char *http_response = (char *)malloc(RESSIZE);
         char *date = get_date_for_server();
+        int content_length = strlen(response);
+        int len, bytes_sent;
+        int n =
+            snprintf(http_response, RESSIZE,
+                     "HTTP/1.1 %s\r\nAcess-Control-Allow-Origin: "
+                     "*\r\nConnection: Keep-Alive\r\nDate: %s\r\nContent-Type: "
+                     "%s\r\nContent-Length: %d\r\n\n%s",
+                     statuscode, date, "text/html", content_length, response);
+        len = strlen(http_response);
+        bytes_sent = send(newfd, http_response, len, 0);
+        shutdown(newfd, SHUT_WR);
 
-        struct file_data *fdata = NULL;
-        fdata = read_file("image.jpg");
-        if(fdata == NULL)
-            fprintf(stderr,"Error opening binary file");
-   
+        free(date);
+        free(http_response);
+      }
+
+    } else if (strcmp(ext, ".css") == 0) {
+      response = content(path);
+      char *statuscode = "200 OK";
+      if (strcmp(response, "404 Not Found") == 0)
+        statuscode = "404 Not Found";
+      char *http_response = (char *)malloc(RESSIZE);
+      char *date = get_date_for_server();
+      int content_length = strlen(response);
+      int len, bytes_sent;
+      int n =
+          snprintf(http_response, RESSIZE,
+                   "HTTP/1.1 %s\r\nAcess-Control-Allow-Origin: "
+                   "*\r\nConnection: Keep-Alive\r\nDate: %s\r\nContent-Type: "
+                   "%s\r\nContent-Length: %d\r\n\n%s",
+                   statuscode, date, "text/css", content_length, response);
+      len = strlen(http_response);
+      bytes_sent = send(newfd, http_response, len, 0);
+      shutdown(newfd, SHUT_WR);
+
+      free(date);
+      free(http_response);
+    }
+
+    else if (strcmp(ext, ".jpg") == 0 || strcmp(ext, ".jpeg") == 0 ||
+             strcmp(ext, ".png") == 0) {
+      char http_header[256];
+      char *date = get_date_for_server();
+
+      struct file_data *fdata = NULL;
+      fdata = read_file(path);
+      if (fdata == NULL) {
+        fprintf(stderr, "Error opening binary file");
+        char *temp404 = "404 Not Found";
+        int templen = strlen(temp404);
+        char tempresponse[256];
+        int temp_n =
+            snprintf(tempresponse, 256,
+                     "HTTP/1.1 404 Not Found\r\nAcess-Control-Allow-Origin: "
+                     "*\r\nConnection: Keep-Alive\r\nDate: %s\r\nContent-Type: "
+                     "%s\r\nContent-Length: %d\r\n\n%s",
+                     date, "text/html", templen, temp404);
+        int temp_bytes_sent =
+            send(newfd, tempresponse, strlen(tempresponse), 0);
+        temp404 = NULL;
+      }
+
+      else {
         uint8_t buff[fdata->length];
-        snprintf(http_header,256,"HTTP/1.1 200 OK\r\nContent-Type: image/jpeg\r\nContent-Length: %llu\r\n\r\n",fdata->length);
-        send(newfd,http_header,strlen(http_header),0);
-        send(newfd,fdata->data,fdata->length,0);
-        printf("length is %ld\n",fdata->length);
+        snprintf(http_header, 256,
+                 "HTTP/1.1 200 OK\r\nContent-Type: "
+                 "image/jpeg\r\nContent-Length: %llu\r\n\r\n",
+                 fdata->length);
+        send(newfd, http_header, strlen(http_header), 0);
+        send(newfd, fdata->data, fdata->length, 0);
         shutdown(newfd, SHUT_WR);
-        fdata = NULL;
 
+        fdata = NULL;
+      }
+
+    } else {
+
+      shutdown(newfd, SHUT_WR);
     }
-    else
-        shutdown(newfd, SHUT_WR);
-    }
-    close(sockfd);
-    return 0;
+  }
+
+  close(sockfd);
+  return 0;
 }
